feb4ebb04f52690ad425de8404facf82ee4f6be2
KlimShaman <klimagapov13@mail.ru>
2023-02-18 12:11:11 +0400

    time in UTC+4

--- Examples/getHistoricalData.py
+++ Examples/getHistoricalData.py
@@ -6,7 +6,11 @@ from datetime import datetime
 
 def getHistData(exchange, ticker, tf, dateFrom, dateTo):   
     apProvider = AlorPy(Config.UserName, Config.RefreshToken)  # Подключаемся к торговому счету. Логин и Refresh Token берутся из файла Config.py
-    hist = apProvider.GetHistory(exchange, ticker, tf, secondsFrom=apProvider.MskDatetimeToUTCTimeStamp(datetime.fromisoformat(dateFrom)), secondsTo=apProvider.MskDatetimeToUTCTimeStamp(datetime.fromisoformat('dateTo')), untraded=False)
+    hist = apProvider.GetHistory(exchange, ticker, tf, secondsFrom=apProvider.MskDatetimeToUTCTimeStamp(datetime.fromisoformat(dateFrom)), secondsTo=apProvider.MskDatetimeToUTCTimeStamp(datetime.fromisoformat(dateTo)), untraded=False)
     histdf = pd.DataFrame(columns = ['time', 'close', 'open', 'high', 'low', 'volume'])
     histdf = histdf.append(hist['history'], ignore_index=True)
+    for i in range(len(histdf['time'])):
+        histdf['time'][i] = apProvider.UTCTimeStampToMskDatetime(histdf['time'][i])
     return histdf
+
+print(getHistData('MOEX', 'SBER', '60', '2023-02-16T00:05:23', '2023-02-17T00:05:23'))
